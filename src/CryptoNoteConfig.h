7ab867582e537f15f237adc9a7e1955e654673ed
Admin <scriptexpert@users.noreply.github.com>
2016-05-30 13:40:53 -0400

    add GENESIS

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "bikercoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001ffffffffffff07029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101aa19a777806b4a9e283d482053620a44af0e09b21a8f7f6b4ebc880dcf8da9e2";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
